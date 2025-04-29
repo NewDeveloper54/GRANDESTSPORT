5591cee40fc3c29edb4ad1e87ecedcd8ea54d907
Amar AMEDJKANE <amedjkane.amar02@gmail.com>
2025-04-29 15:00:32 +0200

    grandest changes

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -3,19 +3,24 @@ import "./Main.css";
 import Notaire from "../assets/logoNotaire.png";
 import mp44 from "../assets/mp44.png";
 import donation from "../assets/acteDonation.png";
-import notaire from "../assets/notaire.png";
-import territoriale from "../assets/territoriale.png";
 import immobilierNotaire from "../assets/immobilierNotaire.png";
 import successionNotaire from "../assets/successionNotaire.png";
 import entreprise from "../assets/entreprise.png";
-import familleNotaire from "../assets/familleNotaire.png";
-import formation from "../assets/formation.png";
-import reglementationNotaire from "../assets/regleNotaire.png";
+import GES from "../assets/GES.svg";
 
 const Main = () => {
   const [animationDone, setAnimationDone] = useState(false);
   const [sectionTwoShowed, setSectionTwoShowed] = useState(false);
   const [NotaireShow, setNotaireShow] = useState(false);
+  const [img2, setImg2] = useState(false);
+
+
+ useEffect(()=>{
+  const timer  =setTimeout(()=>{
+    setImg2(true);
+  },3000);
+ }, []);
+
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -47,7 +52,7 @@ const Main = () => {
             <img
               width="180"
               height="170"
-              src={Notaire}
+              src={GES}
               alt="nothing yet"
               className={animationDone ? "logoAnimate" : "logoHold"}
             />
@@ -55,6 +60,8 @@ const Main = () => {
 
           <div className={`byMp44 ${NotaireShow ? "show" : ""}`}>
             <p>by</p>
+
+
 
             <img src={mp44} height="80" width="80" alt="" />
           </div>
@@ -66,7 +73,7 @@ const Main = () => {
           <a href="https://www.notaires.fr/fr/donation">
             <div className="item one">
               <img src={donation} height="120" width="130" alt="" />
-              <h1>DONATION</h1>
+              <h1>PLAN D'ACCES</h1>
             </div>
           </a>
 
@@ -76,14 +83,14 @@ const Main = () => {
           >
             <div className="item two">
               <img src={successionNotaire} alt="" height="130" width="180" />
-              <h1 className="angled-shadow">SUCCESSION</h1>
+              <h1 className="angled-shadow">CONTACT</h1>
             </div>
           </a>
 
           <a href="https://www.notaires.fr/fr/immobilier">
             <div className="item three">
               <img src={immobilierNotaire} alt="" height="130" width="160" />
-              <h1>IMMOBILIER</h1>
+              <h1>OFFRE SPORTIVE</h1>
             </div>
           </a>
 
@@ -93,62 +100,7 @@ const Main = () => {
           >
             <div className="item four">
               <img src={entreprise} alt="" height="130" width="100" />
-              <h1 className="angled-shadow">ENTREPRISE</h1>
-            </div>
-          </a>
-
-          <a
-            href="https://www.notaires.fr/fr/famille"
-            rel="noopener noreferrer"
-          >
-            <div className="item five">
-              <img src={familleNotaire} alt="" height="130" width="130" />
-              <h1 className="angled-shadow">Famille</h1>
-            </div>
-          </a>
-
-          <a
-            href="https://www.notaires.fr/fr/formation-et-emplois"
-            rel="noopener noreferrer"
-          >
-            <div className="item six">
-              <img src={formation} alt="" height="130" width="130" />
-              <h1 className="angled-shadow">Formation et emploi</h1>
-            </div>
-          </a>
-
-          <a
-            href="https://www.notaires.fr/fr/collectivites-territoriales"
-            rel="noopener noreferrer"
-          >
-            <div className="item seven">
-              <img src={territoriale} alt="" height="200" width="150" />
-              <h1 className="angled-shadow">Collectivités territoriales</h1>
-            </div>
-          </a>
-
-          <a
-            href="https://www.notaires.fr/fr/le-role-du-notaire"
-            rel="noopener noreferrer"
-          >
-            <div className="item eight">
-              <img src={notaire} height="180" width="130" alt="" />
-              <h1 className="angled-shadow">rôle du notaire</h1>
-            </div>
-          </a>
-
-          <a
-            href="https://www.csn.notaires.fr/fr/reglement-national-reglement-inter-cours"
-            rel="noopener noreferrer"
-          >
-            <div className="item nine">
-              <img
-                src={reglementationNotaire}
-                alt=""
-                height="170"
-                width="170"
-              />
-              <h1 className="angled-shadow">Réglementation</h1>
+              <h1 className="angled-shadow">MES MAISONS DELA REGION</h1>
             </div>
           </a>
         </div>
@@ -159,6 +111,22 @@ const Main = () => {
           </div>
         </a>
       </section>
+
+
+
+      <div className="outerbox1"></div>
+      <div className="outerbox2"></div>
+      <img
+              width="615"
+              height="200"
+              src={GES}
+              alt="nothing yet"
+              className={`img2 ${img2 ? "goUp" : ""} `}
+            />
+
+
+
+
     </div>
   );
 };
